2a1165239eb373452140454656899768511e994b
Tarikul islam <tarikul.cse5.bu@gmail.com>
2022-01-27 00:16:00 +0600

    moddify slider and choice list

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -46,3 +46,146 @@ var deadline = new Date(Date.parse(new Date()) +24 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
 
 // Counter Ends
+
+// ___________________SLIDER START___________________________
+let slide_data = [
+  {
+    'src':'https://images.unsplash.com/photo-1595366266798-9752bd56c375?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+    'title':'Gift Ideas For Her',
+    'copy':'Valentines Day Gifts With Delivery.'
+  },
+  {
+    'src':'https://images.unsplash.com/photo-1512909006721-3d6018887383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=570&q=80', 
+    'title':'Kris Kringle Gifts ',
+    'copy':'Everyone loves the time-honoured tradition of the Kris Kringle Christmas gift exchange..'
+  },
+  {
+    'src':'https://images.unsplash.com/photo-1638836369409-667f6d315757?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=450&h=400&q=80', 
+    'title':'The Best Secret Santa Gift',
+    'copy':"It's that time of year again! Time to get your shopping done for the annual office secret Santa again."
+  },
+  {
+    'src':'https://images.unsplash.com/photo-1545785028-23ee5937cf69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=569&q=80', 
+    'title':'Gift Ideas For Older Parents',
+    'copy':'thinking up gift ideas for older parents can prove to be much more difficult than years ago when we were all kids and a messy crayon-filled card and an enthusiastic hug could suffice.'
+  },
+  
+];
+let slides = [],captions = [];
+
+let autoplay = setInterval(function(){
+  nextSlide();
+},5000);
+let slidecontainer = document.getElementById('slidecontainer');
+let leftSlider = document.getElementById('left-col');
+// console.log(leftSlider);
+let down_button = document.getElementById('down_button');
+// let caption = document.getElementById('slider-caption');
+// let caption_heading = caption.querySelector('caption-heading');
+
+down_button.addEventListener('click',function(e){
+  e.preventDefault();
+  clearInterval(autoplay);
+  nextSlide();
+  autoplay;
+});
+
+for (let i = 0; i< slide_data.length; i++){
+  let slide = document.createElement('div'),
+      caption = document.createElement('div'),
+      slide_title = document.createElement('div');
+    
+  slide.classList.add('slide');
+  slide.setAttribute('style','background:url('+slide_data[i].src+');height:100%;');
+  caption.classList.add('caption');
+  slide_title.classList.add('caption-heading');
+  slide_title.innerHTML = '<h1>'+slide_data[i].title+'</h1>';
+  
+  switch(i){
+    case 0:
+        slide.classList.add('current');
+        caption.classList.add('current-caption');
+        break;
+    case 1:
+        slide.classList.add('next');
+        caption.classList.add('next-caption');
+        break;
+    case slide_data.length -1:
+      slide.classList.add('previous');
+      caption.classList.add('previous-caption');
+      break;
+    default:
+       break;       
+  }
+  caption.appendChild(slide_title);
+  caption.insertAdjacentHTML('beforeend','<div class="caption-subhead"><span>dolor sit amet, consectetur adipiscing elit. </span></div>');
+  slides.push(slide);
+  captions.push(caption);
+  leftSlider.appendChild(slide);
+  slidecontainer.appendChild(caption);
+}
+// console.log(slides);
+
+function nextSlide(){
+  // caption.classList.add('offscreen');
+  
+  slides[0].classList.remove('current');
+  slides[0].classList.add('previous','change');
+  slides[1].classList.remove('next');
+  slides[1].classList.add('current');
+  slides[2].classList.add('next');
+  let last = slides.length -1;
+  slides[last].classList.remove('previous');
+  
+  captions[0].classList.remove('current-caption');
+  captions[0].classList.add('previous-caption','change');
+  captions[1].classList.remove('next-caption');
+  captions[1].classList.add('current-caption');
+  captions[2].classList.add('next-caption');
+  let last_caption = captions.length -1;
+  
+  // console.log(last);
+  captions[last].classList.remove('previous-caption');
+  
+  let placeholder = slides.shift();
+  let captions_placeholder = captions.shift();
+  slides.push(placeholder); 
+  captions.push(captions_placeholder);
+}
+
+let heading = document.querySelector('.caption-heading');
+
+
+// https://jonsuh.com/blog/detect-the-end-of-css-animations-and-transitions-with-javascript/
+function whichTransitionEvent(){
+  var t,
+      el = document.createElement("fakeelement");
+
+  var transitions = {
+    "transition"      : "transitionend",
+    "OTransition"     : "oTransitionEnd",
+    "MozTransition"   : "transitionend",
+    "WebkitTransition": "webkitTransitionEnd"
+  }
+
+  for (t in transitions){
+    if (el.style[t] !== undefined){
+      return transitions[t];
+    }
+  }
+}
+
+var transitionEvent = whichTransitionEvent()
+caption.addEventListener(transitionEvent, customFunction);
+
+function customFunction(event) {
+  caption.removeEventListener(transitionEvent, customFunction);
+  console.log('animation ended');
+
+  // Do something when the transition ends
+}
+
+
+
+
+// ___________________SLIDER END_______________________________
